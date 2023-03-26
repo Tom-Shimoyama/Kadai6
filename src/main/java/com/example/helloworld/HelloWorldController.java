733d1abe82a7a498409ec9022dd8c7903de8a9dd
Tom-Shimoyama <125033423+Tom-Shimoyama@users.noreply.github.com>
2023-03-26 17:50:54 +0900

    第６回課題の自分で考えた変更を加えました。

--- src/main/java/com/example/helloworld/HelloWorldController.java
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -5,9 +5,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    @GetMapping("/fortune")
+    public String start(){
+        double fn = Math.random();
+        if (fn >= 0.7){
+            return "あなたの運勢は大吉です！！！";
+        } else if (fn >= 0.4){
+            return "あなたの運勢は中吉です！！";
+        } else if (fn >= 0.1){
+            return "あなたの運勢は小吉です";
+        } else {
+            return "あなたの運勢は凶です！！";
+        }
     }
-
 }
